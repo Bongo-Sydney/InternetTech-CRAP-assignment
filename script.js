e57ea73b255af6a66346c3b9cb137a3dc04d8423
Bongo-Sydney <bongomeshack@gmail.com>
2025-01-20 14:18:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.login-button');
+const iconClose = document.querySelector('.icon-close');
+const mainttext = document.querySelector('.content-main')
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+    mainttext.style.display = 'none';
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+
+    setTimeout(() => {
+        mainttext.style.display = 'block';
+    }, 1000); 
+});
